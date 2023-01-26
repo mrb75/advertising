ff93a0d5218a1420fd67b082ca03512fa234135b
mrb75 <mrbakhtiari4@yahoo.com>
2023-01-26 13:02:24 +0330

    admin for ads and ad ctegory created.

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Ad, Category, Property, Category, AdImage
 
-# Register your models here.
+
+class PropertyInline(admin.TabularInline):
+    model = Ad.properties.through
+
+
+class ChildInline(admin.TabularInline):
+    model = Category
+    fk_name = 'parent'
+    verbose_name = 'child'
+
+
+class ImageInline(admin.TabularInline):
+    model = AdImage
+    fk_name = 'ad'
+
+
+class AdAdmin(admin.ModelAdmin):
+    inlines = [
+        PropertyInline,
+        ImageInline,
+    ]
+    display = ['title', 'category', 'user', 'description', 'is_verified',
+               'price', 'properties', 'create_date_time', 'modify_date_time']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        ChildInline,
+    ]
+    display = ['name', 'parent', 'create_date_time', 'modify_date_time']
+
+
+admin.site.register(Ad, AdAdmin)
+admin.site.register(Category, CategoryAdmin)
